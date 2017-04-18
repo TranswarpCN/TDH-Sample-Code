5a397f808c202c917798c0b99ee9e1b83141b6a5
SSSSQD <shaoqidong@hotmail.com>
2017-04-18 15:08:10 +0800

    renew project 0418a

--- KafkaConsumer/src/main/java/io/transwarp/kafkaconsumer/SampleCode.java
+++
@@ -1,9 +0,0 @@
-package io.transwarp.kafkaConsumer;
-
-public class SampleCode {
-    // 主函数
-    public static void main(String[] args) {
-        kafkaConsumer kafkaConsumer = new kafkaConsumer();
-        kafkaConsumer.go();
-    }
-}
